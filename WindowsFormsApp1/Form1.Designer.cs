00f2f3b3f91ef33b8ecccba81f5175df22d79e58
Haradorn <shewingtrue@gmail.com>
2020-02-15 20:24:38 +0300

    Начальная перестановка
    Этап начальной перестановки по массиву

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Исходный текст";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(592, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 54);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Зашифровать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(21, 148);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(357, 593);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox2.Location = new System.Drawing.Point(407, 148);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(357, 593);
+            this.richTextBox2.TabIndex = 5;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox3.Location = new System.Drawing.Point(798, 148);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(357, 593);
+            this.richTextBox3.TabIndex = 6;
+            this.richTextBox3.Text = "";
+            this.richTextBox3.TextChanged += new System.EventHandler(this.richTextBox3_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(404, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 21);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Коды";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(795, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(160, 21);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Обработанный текст";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(409, 74);
+            this.textBox1.MaxLength = 8;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(115, 25);
+            this.textBox1.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(405, 40);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 21);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Ключ";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(21, 45);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 54);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Открыть файл";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(203, 45);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(175, 54);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Сохранить файл";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(406, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1177, 761);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Программа для шифрования";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private string fn = string.Empty;
+        private bool docChanged = false;
+        //матрица начальной перестановки
+        public int[] arrStart = new int[]{58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4, 62, 54, 46, 38, 30, 22, 14, 6, 64, 56, 48, 40, 32, 24, 16, 8,
+                                           57, 49, 41, 33, 25, 17, 9, 1, 59, 51, 43, 35, 27, 19, 11, 3, 61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7};
+        public Form1()
+        {
+            InitializeComponent();
+            openFileDialog1.DefaultExt = "txt";
+            openFileDialog1.Filter = "текст|*.txt";
+            openFileDialog1.Title = "Открыть сообщение";
+            openFileDialog1.Multiselect = false;
+            saveFileDialog1.DefaultExt = "txt";
+            saveFileDialog1.Filter = "текст|*.txt";
+            saveFileDialog1.Title = "Сохранить сообщение";
+        }
+        private void OpenDocument()
+        {
+            openFileDialog1.FileName = string.Empty;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                fn = openFileDialog1.FileName;
+                this.Text = fn;
+                try
+                {
+                    System.IO.StreamReader sr =
+                        new System.IO.StreamReader(fn, Encoding.GetEncoding(1251));
+                    richTextBox1.Text = sr.ReadToEnd();
+                    richTextBox1.SelectionStart =
+                            richTextBox1.TextLength;
+                    sr.Close();
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show("Ошибка доступа к файлу\n" +
+                        exc.ToString(), "CryptoProgram",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
+            }
+        }
+        private int SaveDocument()
+        {
+            int result = 0;
+            if (fn == string.Empty)
+            {
+                if (saveFileDialog1.ShowDialog() ==
+                            DialogResult.OK)
+                {
+                    fn = saveFileDialog1.FileName;
+                    this.Text = fn;
+                }
+                else result = -1;
+            }
+            if (fn != string.Empty)
+            {
+                try
+                {
+                    System.IO.FileInfo fi =
+                        new System.IO.FileInfo(fn);
+                    System.IO.StreamWriter sw =
+                        fi.CreateText();
+                    sw.Write(richTextBox3.Text);
+                    sw.Close();
+                    result = 0;
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show(exc.ToString(),
+                            "CryptoProgram",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Error);
+                }
+            }
+            return result;
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Text = "";
+            richTextBox3.Text = "";
+            //string strKey - ключ
+            string strKey = textBox1.Text;
+            //string str исходный текст;
+            string str = richTextBox1.Text;
+            //переводим символы исходного текста в их ASCII код
+            byte[] bytes = Encoding.Default.GetBytes(str);
+            //выводим коды символов
+            richTextBox2.Text += "ASCII коды исходного текста\n";
+            for (int i = 0; i < bytes.Length; i++)
+                richTextBox2.Text += bytes[i] + " ";
+            string[] strArr = new string[bytes.Length];
+            richTextBox2.Text += "\n";
+            //конвертируем ASCII коды в двоичную систему, представление восьмиразрядное
+            //при необходимости дополняем слева нулями
+            for(int i = 0; i < strArr.Length; i++)
+            {
+                strArr[i] = Convert.ToString(bytes[i], 2).PadLeft(8, '0');
+            }
+            //объявляем int-массив, равный по длине строковому массиву, умноженному на восемь
+            int[] intArr = new int[strArr.Length*8];
+            //переводим символы исходного ключа в их ASCII код
+            byte[] bytesKey = Encoding.Default.GetBytes(strKey);
+            //выводим коды символов ключа
+            richTextBox2.Text += "ASCII коды ключа\n";
+            for (int i = 0; i < bytesKey.Length; i++)
+                richTextBox2.Text += bytesKey[i] + " ";
+            richTextBox2.Text += "\n";
+            string[] strArrKey = new string[bytesKey.Length];
+            //конвертируем ASCII коды в двоичную систему, представление восьмиразрядное
+            //при необходимости дополняем слева нулями
+            for (int i = 0; i < strArrKey.Length; i++)
+            {
+                strArrKey[i] = Convert.ToString(bytesKey[i], 2).PadLeft(8, '0');
+            }
+            int[] intArrKey = new int[strArrKey.Length * 8];
+            richTextBox2.Text += "Биты исходного текста";
+            richTextBox2.Text += "\n";
+            //записываем в числовой массив все биты исходного текста
+            int j = 0;
+            while(j != intArr.Length)
+            {
+                for (int i = 0; i < strArr.Length; i++)
+                {
+                    foreach (char ch in strArr[i])
+                    {
+                        intArr[j] = (int)Char.GetNumericValue(ch);
+                        richTextBox2.Text += intArr[j];
+                        j++;
+                    }
+                    richTextBox2.Text += " ";
+                }
+            }
+            int len = intArr.Length;
+            if(len%64 != 0)
+            {
+                while(len%64!=0)
+                {
+                    len++;
+                }
+            }
+            richTextBox2.Text += len + "\n";
+            //создаём расширяющий массив до длины, чтобы можно было поделить на равные блоки по 64 бита
+            //пока счётчик индексов меньше длины исходного битового массива, в расширяющий массив записываем
+            //его значения, а потом заполняем нулями
+            int[] intArrExtended = new int[len];
+            for (int i = 0; i < intArrExtended.Length; i++)
+            {
+                if (i < intArr.Length) { intArrExtended[i] = intArr[i]; }
+                else intArrExtended[i] = 0;
+            }
+            for(int i = 0; i < intArrExtended.Length; i++)
+            {
+                richTextBox2.Text += intArrExtended[i];
+            }
+            int[] intArrExtended2 = new int[intArrExtended.Length];
+            for(int i = 0; i < intArrExtended2.Length; i++)
+            {
+                intArrExtended2[i] = intArrExtended[arrStart[i]-1];
+            }
+            richTextBox2.Text += "\n";
+            for (int i = 0; i < intArrExtended2.Length; i++)
+            {
+                richTextBox2.Text += intArrExtended2[i];
+            }
+
+
+
+
+
+            j = 0;
+            /*richTextBox2.Text += "\n";
+            richTextBox2.Text += "Биты исходного ключа";
+            richTextBox2.Text += "\n";
+            //записываем в числовой массив все биты исходного ключа
+            while (j != intArrKey.Length)
+            {
+                for (int i = 0; i < strArrKey.Length; i++)
+                {
+                    foreach (char ch in strArrKey[i])
+                    {
+                        intArrKey[j] = (int)Char.GetNumericValue(ch);
+                        richTextBox2.Text += intArrKey[j];
+                        j++;
+                    }
+                    richTextBox2.Text += " ";
+                }
+            }
+
+
+
+
+
+
+
+            //строковый массив strExit - каждый элемент содержит 8 бит числового массива исходного текста
+            string[] strExit = new string[strArr.Length];
+            //переписываем содержимое числового массива в строковый (нужно для вывода текста)
+            //каждые восемь бит в отдельный элемент строкового массива
+            int count = 0;
+            for(int i = 0; i < strExit.Length; i++)
+            {
+                for(int k = count; k < intArr.Length; k++)
+                {
+                    if((k>0)&&(k%8==0))
+                    {
+                        strExit[i+1] += intArr[k];
+                        count = k+1;
+                        break;
+                    }
+                    strExit[i] += intArr[k];
+                }
+            }
+            int[] intArr2 = new int[strExit.Length];
+            //конвертируем обратно в int-массив
+            for (int i = 0; i < intArr2.Length; i++)
+            {
+                intArr2[i] = Convert.ToInt32(strExit[i]);
+            }
+            int[] intArr3 = new int[intArr2.Length];
+            //теперь переносим сконвертированные десятичные значения в другой целочисленный массив
+            for (int i = 0; i < intArr2.Length; i++)
+            {
+                intArr3[i] = Convert.ToInt32(intArr2[i].ToString(), 2);
+            }
+            byte[] byteArr = new byte[intArr3.Length];
+            //переносим те же значения в byte-массив
+            for (int i = 0; i < byteArr.Length; i++)
+            {
+                byteArr[i] = (byte)intArr3[i];
+            }
+            //переводим ASCII коды byte-массива в их символьное представление
+            string str4 = Encoding.Default.GetString(byteArr);
+            richTextBox3.Text += str4;  */
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = string.Empty;
+            if (openFileDialog1.ShowDialog() ==
+                                DialogResult.OK)
+            {
+                fn = openFileDialog1.FileName;
+                this.Text = fn;
+                try
+                {
+                    System.IO.StreamReader sr =
+                        new System.IO.StreamReader(fn, Encoding.UTF8);
+                    richTextBox1.Text = sr.ReadToEnd();
+                    richTextBox1.SelectionStart =
+                        richTextBox1.TextLength;
+                    sr.Close();
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show("Ошибка чтения файла.\n" +
+                            exc.ToString(), "MEdit",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Error);
+                }
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SaveDocument();
+        }
+        private void richTextBox3_TextChanged(object sender, EventArgs e)
+        {
+            docChanged = true;
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (docChanged)
+            {
+                DialogResult dr;
+                dr = MessageBox.Show("Сохранить изменения?",
+                                "Crypto Program",
+                                MessageBoxButtons.YesNoCancel,
+                                MessageBoxIcon.Warning);
+                switch (dr)
+                {
+                    case DialogResult.Yes:
+                        if (SaveDocument() != 0)
+                            e.Cancel = true;
+                        break;
+                    case DialogResult.No:
+                        ;
+                        break;
+                    case DialogResult.Cancel:
+                        e.Cancel = true;
+                        break;
+                }
+            }
+        }
+    }
+}
